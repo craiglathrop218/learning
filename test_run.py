69f54557b0af345b44d31b618d53e1a0a39e5c00
craiglathrop218 <craiglathrop218@gmail.com>
2017-09-06 16:33:29 -0700

    another change

--- test_run.py
+++ test_run.py
@@ -5,5 +5,3 @@ p = WFProjectController(api_key=WorkfrontConfig.api_key,
                         subdomain=WorkfrontConfig.subdomain,
                         env=WorkfrontConfig.env)
 
-p.load_project('59ab1b0200b48d92cfaefb590aa32fc4')
-
